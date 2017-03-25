6f5a703f51621450f2f943571dfc0625196a179e
Patrick Dammann <github@thesinforest.eu>
2017-03-25 19:18:23 +0100

    modified link.sh

--- link.sh
+++ link.sh
@@ -2,9 +2,16 @@
 
 dir=$(pwd)
 
-ln -s $pwd/i3blocks ~/.i3blocks.conf
-ln -s $pwd/i3config ~/.config/i3/config
-ln -s $pwd/Xdefaults ~/.Xdefaults
-ln -s $pwd/xinitrc ~/.xinitrc
-ln -s $pwd/xsessionrc ~/.xsessionrc
-ln -s $pwd/zshrc ~/.zshrc
+rm ~/.i3blocks.conf
+rm ~/.config/i3/config
+rm ~/.Xdefaults
+rm ~/.xinitrc
+rm ~/.xsessionrc
+rm ~/.zshrc
+
+ln -s "$pwd/i3blocks" ~/.i3blocks.conf
+ln -s "$pwd/i3config" ~/.config/i3/config
+ln -s "$pwd/Xdefaults" ~/.Xdefaults
+ln -s "$pwd/xinitrc" ~/.xinitrc
+ln -s "$pwd/xsessionrc" ~/.xsessionrc
+ln -s "$pwd/zshrc" ~/.zshrc
