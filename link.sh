f73f420b0f58f509860bc5a4a489665d1f2ee462
Patrick Dammann <github@thesinforest.eu>
2017-03-25 19:11:59 +0100

    added link.sh

---
+++ link.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+dir=$(pwd)
+
+ln -s $pwd/i3blocks ~/.i3blocks.conf
+ln -s $pwd/i3config ~/.config/i3/config
+ln -s $pwd/Xdefaults ~/.Xdefaults
+ln -s $pwd/xinitrc ~/.xinitrc
+ln -s $pwd/xsessionrc ~/.xsessionrc
+ln -s $pwd/zshrc ~/.zshrc
