2c0f6259786bad15049a35fe278f542e3b67c0a8
Patrick Dammann <github@thesinforest.eu>
2017-03-25 19:31:35 +0100

    modified link.sh again

--- link.sh
+++ link.sh
@@ -7,9 +7,9 @@ rm ~/.xinitrc
 rm ~/.xsessionrc
 rm ~/.zshrc
 
-ln -s "~/.dotfiles/i3blocks" ~/.i3blocks.conf
-ln -s "~/.dotfiles/i3config" ~/.config/i3/config
-ln -s "~/.dotfiles/Xdefaults" ~/.Xdefaults
-ln -s "~/.dotfiles/xinitrc" ~/.xinitrc
-ln -s "~/.dotfiles/xsessionrc" ~/.xsessionrc
-ln -s "~/.dotfiles/zshrc" ~/.zshrc
+ln -s .dotfiles/i3blocks ~/.i3blocks.conf
+ln -s .dotfiles/i3config ~/.config/i3/config
+ln -s .dotfiles/Xdefaults ~/.Xdefaults
+ln -s .dotfiles/xinitrc ~/.xinitrc
+ln -s .dotfiles/xsessionrc ~/.xsessionrc
+ln -s .dotfiles/zshrc ~/.zshrc
