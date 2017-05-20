4478ab75769e020711a7e3291735106273655d84
Patrick Dammann <github@thesinforest.eu>
2017-05-20 12:50:17 +0200

    changed setup from urxvt to termite

--- link.sh
+++ link.sh
@@ -3,13 +3,19 @@
 rm ~/.i3blocks.conf
 rm ~/.config/i3/config
 rm ~/.Xdefaults
+rm ~/.Xresources
 rm ~/.xinitrc
 rm ~/.xsessionrc
 rm ~/.zshrc
+rm ~/.compton.conf
+rm ~/.config/termite
 
 ln -s ~/.dotfiles/i3blocks ~/.i3blocks.conf
 ln -s ~/.dotfiles/i3config ~/.config/i3/config
 ln -s ~/.dotfiles/Xdefaults ~/.Xdefaults
+ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/xinitrc ~/.xinitrc
 ln -s ~/.dotfiles/xsessionrc ~/.xsessionrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/compton ~/.compton.conf
+ln -s ~/.dotfiles/termite ~/.config/termite
